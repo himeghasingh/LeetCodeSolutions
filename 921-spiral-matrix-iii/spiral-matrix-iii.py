6671318f9e3235b2bd6f76675c31916154743101
Megha Singh <145432042+himeghasingh@users.noreply.github.com>
2024-08-09 03:37:37 -0700

    Time: 77 ms (79.78%) | Memory: 17.9 MB (13.60%) - LeetSync

--- 921-spiral-matrix-iii/spiral-matrix-iii.py
+++ 921-spiral-matrix-iii/spiral-matrix-iii.py
@@ -5,14 +5,9 @@ class Solution:
         add = 1
         turn = 1
         result = [[rStart, cStart]]
-        while (count < total):
-            # print("\n")
-            
+        while (count < total):            
             if turn % 4 == 1:
                 for j in range(cStart+1, cStart+add+1):
-                    # print("turn", turn)
-                    # print("\nj", j)
-                    # print("rstart", rStart)
                     if 0 <= rStart < rows and 0 <= j < cols:
                         result.append([rStart,j])
                         count += 1
@@ -20,9 +15,6 @@ class Solution:
 
             if turn % 4 == 2:
                 for i in range(rStart+1, rStart+add+1):
-                    # print("turn", turn)
-                    # print("\ni", i)
-                    # print("cstart", cStart)
                     if 0 <= i < rows and 0 <= cStart < cols:
                         result.append([i, cStart])
                         count += 1
@@ -30,20 +22,12 @@ class Solution:
 
             if turn % 4 == 3:
                 for j in range(cStart-1, cStart-1-add, -1):
-                    # print("turn", turn)
-                    # print("\nj", j)
-                    # print("rstart", rStart)
                     if 0 <= rStart < rows and 0 <= j < cols:
                         result.append([rStart, j])
                         count += 1
                 cStart = cStart - add
 
             if turn % 4 == 0:
-                # print("add", add)
-                # print("rstart", rStart)
-                # print("cstart", cStart)
-                # print("rstart-1", rStart-1)
-                # print("rstart-add-1", rStart-add-1)
                 for i in range(rStart-1, rStart-add-1, -1):
 
                     if 0 <= i < rows and 0 <= cStart < cols:
@@ -55,8 +39,6 @@ class Solution:
                 add += 1
             turn += 1
             
-
-        # print(result)
         return result
 
 
