6f06083b027e99f0433e9cae8dacb85939023281
Megha Singh <145432042+himeghasingh@users.noreply.github.com>
2024-08-09 20:53:26 -0700

    Time: 209 ms (18.97%) | Memory: 27.9 MB (5.14%) - LeetSync

--- 999-regions-cut-by-slashes/regions-cut-by-slashes.py
+++ 999-regions-cut-by-slashes/regions-cut-by-slashes.py
@@ -16,26 +16,24 @@ class Solution:
                     scaledGrid[(3*i)+2][(3*j)+2] = 1
 
         
-        directions = [[1,0],[-1,0],[0,1],[0,-1]]       
+        directions = [[1,0],[-1,0],[0,1],[0,-1]]    
+        visited = set()     
+        regions = 0   
 
-        def regionCount(iindex, jindex, visited):
+        def regionCount(iindex, jindex):
             visited.add((iindex, jindex))
             for idir, jdir in directions:
                 new_i = iindex + idir
                 new_j = jindex + jdir
                 if (0 <= new_i < new_n) and (0 <= new_j < new_n) and (scaledGrid[new_i][new_j] == 0) and ((new_i, new_j) not in visited):
-                    regionCount(new_i, new_j, visited)
+                    regionCount(new_i, new_j)
                 
-
-        visited = set()     
-        regions = 0
         for i in range (0, new_n):
             for j in range (0, new_n):
                 if scaledGrid[i][j] == 0 and (i,j) not in visited:
-                    regionCount(i, j, visited)
+                    regionCount(i, j)
                     regions += 1
                 
-
         return regions
 
                 
