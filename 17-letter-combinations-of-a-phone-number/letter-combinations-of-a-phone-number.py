765153b55985a421eced9d3d95db97eb5f42d843
Megha Singh <145432042+himeghasingh@users.noreply.github.com>
2024-08-13 03:48:10 -0700

    Time: 40 ms (21.51%) | Memory: 16.5 MB (74.16%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,25 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        n = len(digits)
+        letterNumMap = {"2":["a", "b", "c"], "3":["d", "e", "f"], "4":["g", "h", "i"], "5":["j", "k", "l"], "6":["m", "n", "o"], "7":["p", "q", "r", "s"], "8":["t", "u", "v"], "9":["w", "x", "y", "z"]}
+        res = []
+
+        def makeComb(index, pattern):
+            if index == n:
+                res.append(pattern)
+                return
+            currNum = digits[index]
+            currLetters = letterNumMap[currNum]
+            for letter in currLetters:
+                makeComb(index + 1, pattern + letter)
+
+        makeComb(0, "")
+        print(res)
+        return res
+
+            
+
+
+        
