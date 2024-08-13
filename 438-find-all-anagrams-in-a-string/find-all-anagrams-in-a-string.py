02dfd2195df82858a1f2aca167344eea693117a9
Megha Singh <145432042+himeghasingh@users.noreply.github.com>
2024-08-13 03:32:33 -0700

    Time: 287 ms (20.42%) | Memory: 17.8 MB (24.23%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,13 +1,42 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        sorted_p = sorted(p)
-        n_p = len(p)
-        n = len(s)
+        ns = len(s)
+        np = len(p)
+        if np > ns:
+            return []
+        pCounter = Counter(p)
+        sCounter = Counter()
         res = []
-        for i in range(0, n - n_p + 1):
-            if sorted(s[i:i+n_p]) == sorted_p:
-                res.append(i)
-        print(res)
+
+        for i in range(0, ns):
+            sCounter[s[i]] += 1
+            if i >= np:
+                sCounter[s[i-np]] -= 1
+                if sCounter[s[i-np]] == 0:
+                    del sCounter[s[i-np]]
+            if sCounter == pCounter:
+                res.append(i - np + 1)
+
         return res
+
+
+
+
+
+
+
+
+
+
+
+        # Beats only 7%, not efficient
+        # sorted_p = sorted(p)
+        # n_p = len(p)
+        # n = len(s)
+        # res = []
+        # for i in range(0, n - n_p + 1):
+        #     if sorted(s[i:i+n_p]) == sorted_p:
+        #         res.append(i)
+        # return res
 
         
