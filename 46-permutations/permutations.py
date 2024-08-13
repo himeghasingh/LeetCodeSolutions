350b2c908ec7752e24fa61a4f1bf83c1ab18ecfd
Megha Singh <145432042+himeghasingh@users.noreply.github.com>
2024-08-13 04:12:36 -0700

    Time: 86 ms (5.95%) | Memory: 16.9 MB (6.27%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,19 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        res = []
+
+        def makeComb(startInd, path):
+            if len(path) == n and path not in res:
+                print(path)
+                res.append(list(path))
+                return
+            for j in range(0, n):
+                if nums[j] not in path:
+                    makeComb(j+1, path+[nums[j]])
+        
+        for i in range(0, n):
+            makeComb(i, [])
+        return res
+            
+        
