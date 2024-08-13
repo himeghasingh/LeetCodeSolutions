e003c2d432d7f0338d50733f0c67d18735b017a0
Megha Singh <145432042+himeghasingh@users.noreply.github.com>
2024-08-12 23:11:12 -0700

    Time: 67 ms (40.60%) | Memory: 16.6 MB (30.23%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        n = len(candidates)
+        results = []
+
+        def makeSum(target, startInd, path):
+            if target == 0:
+                if path not in results:
+                    results.append(copy.deepcopy(path))
+                    return
+            for i in range(startInd, n):
+                if i > startInd and candidates[i] == candidates[i - 1]:
+                    continue  # Skip duplicates
+                if candidates[i] > target:
+                    return
+                makeSum(target-candidates[i], i+1, path+[candidates[i]]) 
+
+        for i in range(0, n):
+            makeSum(target, i, [])
+        return results
+
+        
+        
