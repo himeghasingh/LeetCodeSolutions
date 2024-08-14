cfe11df0ea1555e11aa1a7f3d2716b403005db9b
Megha Singh <145432042+himeghasingh@users.noreply.github.com>
2024-08-14 08:34:51 -0700

    Time: 39 ms (81.47%) | Memory: 17.1 MB (6.21%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -0,0 +1,47 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        n = len(nums)
+        l = 0
+        r = n - 1
+
+        while (l < r):
+            mid = (l + r) // 2
+            if nums[mid] >= nums[r]:
+                l = mid + 1
+            else:
+                r = mid
+        print("im here")
+        
+        minIndex = l
+        print("minindex", minIndex)
+        if nums[0] <= target <= nums[minIndex-1] and minIndex > 0:
+            print("in first half")
+            left = 0
+            right = minIndex - 1
+            while(left <= right):
+                middle = (left + right) // 2
+                if nums[middle] == target:
+                    return middle
+                elif nums[middle] < target:
+                    left = middle + 1
+                else:
+                    right = middle - 1
+        
+        elif nums[minIndex] <= target <= nums[n-1]:
+            print("in second half")
+            left = minIndex
+            right = n - 1
+            while(left <= right):
+                middle = (left + right) // 2
+                if nums[middle] == target:
+                    return middle
+                elif nums[middle] < target:
+                    left = middle + 1
+                else:
+                    right = middle - 1
+
+        else:
+            return -1
+        return -1
+        
