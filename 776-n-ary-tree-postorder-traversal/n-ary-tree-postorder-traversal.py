8664fe6fa0cec6d0ba7b265ebd841d20fe3ff9da
Megha Singh <145432042+himeghasingh@users.noreply.github.com>
2024-08-26 03:04:46 -0700

    Time: 47 ms (45.79%) | Memory: 18.2 MB (77.39%) - LeetSync

--- 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.py
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.py
@@ -8,15 +8,50 @@ class Node:
 
 class Solution:
     def postorder(self, root: 'Node') -> List[int]:
-        ll = []
+        if not root:
+            return []
+        queue = deque()
+        stack = []
+        stack.append(root)
+        res = []
         
-        def traverse(node):
-            if not node:
-                return
-            for nc in node.children:
-                traverse(nc)
-            ll.append(node.val)
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            for c in node.children:
+                stack.append(c)
+        return res[::-1]
+        
 
-        traverse(root)
-        return ll
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #####Recursive#####
+        # ll = []
+        
+        # def traverse(node):
+        #     if not node:
+        #         return
+        #     for nc in node.children:
+        #         traverse(nc)
+        #     ll.append(node.val)
+
+        # traverse(root)
+        # return ll
         
