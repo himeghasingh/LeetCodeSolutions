623b068406fa25a5eadd6940e9c76dedc52c9626
Megha Singh <145432042+himeghasingh@users.noreply.github.com>
2024-09-08 05:24:58 -0400

    Time: 72 ms (42.65%) | Memory: 17.3 MB (64.08%) - LeetSync

---
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.py
@@ -0,0 +1,52 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
+        
+        def findInTree(value, root, positions):
+            if not root:
+                return 
+            if root.val == value:
+                positions.append(root)
+            findInTree(value, root.left, positions)
+            findInTree(value, root.right, positions)
+            return positions
+
+        headPos = findInTree(head.val, root, [])
+        head = head.next
+        while head:
+            nodePos = []
+            for pos in headPos:
+                if pos.left and pos.left.val == head.val:
+                    nodePos.append(pos.left)
+                if pos.right and pos.right.val == head.val:
+                    nodePos.append(pos.right)
+            if nodePos == []:
+                return False
+            # print("/n")
+            # for n in nodePos:
+            #     print(n.val, " ")
+            headPos = nodePos
+            head = head.next
+                
+     
+        return True
+            
+
+
+
+
+        
+
+
+
+        
