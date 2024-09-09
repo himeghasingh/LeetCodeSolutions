dd26d628984b35fe8d235c0bd92a20cdd47d7160
Megha Singh <145432042+himeghasingh@users.noreply.github.com>
2024-09-09 06:02:12 -0400

    Time: 37 ms (36.96%) | Memory: 16.5 MB (85.58%) - LeetSync

--- 1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.py
+++ 1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.py
@@ -3,11 +3,15 @@ class Solution:
         perminangle = 360 / 60
         minangle = minutes * perminangle
         perhourangle = 360 / 12
+        hour = hour%12
         hourangle = hour * perhourangle
-        if hourangle == 360:
-            hourangle = 0
+        
         smallhrangle = 30 / 60
         smallhrangleval = minutes * smallhrangle
         finalhr = hourangle + smallhrangleval
         return min(abs(finalhr-minangle), 360-abs(finalhr-minangle))
+
+        # elegant cal for hour angle:
+        # hour_angle = (hour % 12 + minutes / 60) * one_hour_angle
+
         
