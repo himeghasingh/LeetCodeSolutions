5c751df4cf1257276574f850d4c9690b94d8935e
Megha Singh <145432042+himeghasingh@users.noreply.github.com>
2024-09-09 01:20:00 -0400

    Time: 25 ms (98.75%) | Memory: 16.4 MB (98.99%) - LeetSync

--- 1400-find-winner-on-a-tic-tac-toe-game/find-winner-on-a-tic-tac-toe-game.py
+++ 1400-find-winner-on-a-tic-tac-toe-game/find-winner-on-a-tic-tac-toe-game.py
@@ -1,36 +1,60 @@
 class Solution:
     def tictactoe(self, moves: List[List[int]]) -> str:
-        matrix = [[0]*3 for i in range(3)]
-        def checkSequence():
-            if (
-                (matrix[0][0] == matrix[0][1] == matrix[0][2] != 0) or
-                (matrix[1][0] == matrix[1][1] == matrix[1][2] != 0) or
-                (matrix[2][0] == matrix[2][1] == matrix[2][2] != 0) or
-
-                (matrix[0][0] == matrix[1][0] == matrix[2][0] != 0) or
-                (matrix[0][1] == matrix[1][1] == matrix[2][1] != 0) or
-                (matrix[0][2] == matrix[1][2] == matrix[2][2] != 0) or
-
-                (matrix[0][0] == matrix[1][1] == matrix[2][2] != 0) or
-                (matrix[2][0] == matrix[1][1] == matrix[0][2] != 0)
-            ):
-                return True
-            else:
-                return False
-                
+        rows = [0] * 3
+        cols = [0] * 3
+        diag = 0
+        antidiag = 0
         player = -1
-        for move in moves:
+        for r, c in moves:
             player = -1 * player
-            x, y = move
-            matrix[x][y] = player
-            if checkSequence():
+            if r == c:
+                diag += player
+            if r + c == 2:
+                antidiag += player
+            rows[r] += player
+            cols[c] += player
+            if (abs(diag) == 3 or abs(antidiag) == 3 or abs(rows[r]) == 3 or abs(cols[c]) == 3):
                 if player == 1:
                     return "A"
                 else:
                     return "B"
         if len(moves) == 9:
             return "Draw"
-        return "Pending" 
+        return "Pending"
+
+
+         # Brute Force
+        # matrix = [[0]*3 for i in range(3)]
+        # def checkSequence():
+        #     if (
+        #         (matrix[0][0] == matrix[0][1] == matrix[0][2] != 0) or
+        #         (matrix[1][0] == matrix[1][1] == matrix[1][2] != 0) or
+        #         (matrix[2][0] == matrix[2][1] == matrix[2][2] != 0) or
+
+        #         (matrix[0][0] == matrix[1][0] == matrix[2][0] != 0) or
+        #         (matrix[0][1] == matrix[1][1] == matrix[2][1] != 0) or
+        #         (matrix[0][2] == matrix[1][2] == matrix[2][2] != 0) or
+
+        #         (matrix[0][0] == matrix[1][1] == matrix[2][2] != 0) or
+        #         (matrix[2][0] == matrix[1][1] == matrix[0][2] != 0)
+        #     ):
+        #         return True
+        #     else:
+        #         return False
+                
+        # player = -1
+        # for move in moves:
+        #     player = -1 * player
+        #     x, y = move
+        #     matrix[x][y] = player
+        #     if checkSequence():
+        #         if player == 1:
+        #             return "A"
+        #         else:
+        #             return "B"
+        # if len(moves) == 9:
+        #     return "Draw"
+        # return "Pending" 
 
         
             
