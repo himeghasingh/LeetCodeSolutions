acc3396784d53333c76d2dc78ebd44f1a3295fca
Megha Singh <145432042+himeghasingh@users.noreply.github.com>
2024-09-09 04:59:32 -0400

    Time: 43 ms (13.80%) | Memory: 16.5 MB (42.91%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        strr = str(abs(x))
+        strr = strr[::-1]
+        if x < 0:
+            strr = '-' + strr
+        newint = int(strr)
+        print(newint)
+        if -1 * ((2**31) - 1) <= newint <= (2**31) + 1:
+            return newint
+        return 0
+
+        
