c8a6b7d4372d7683d7eccd27c719bf91cfec9ead
Megha Singh <145432042+himeghasingh@users.noreply.github.com>
2024-09-09 04:10:32 -0400

    Time: 339 ms (92.16%) | Memory: 17.8 MB (35.85%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -3,7 +3,6 @@ class Solution:
         n = len(nums)
         goal = n - 1
         for i in range(n-2, -1, -1):
-            print("current number ",nums[i])
             if nums[i] >= goal - i :
                 goal = i
         if goal == 0:
