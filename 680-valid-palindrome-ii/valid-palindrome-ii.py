f8557c7e0f1e22c50f53001ec975def3ae9b5f19
Megha Singh <145432042+himeghasingh@users.noreply.github.com>
2024-09-09 05:35:52 -0400

    Time: 68 ms (93.89%) | Memory: 16.9 MB (69.39%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,15 +1,6 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        # count = Counter(s)
-        # print(count)
-        # c = 0
-        # for ch, freq in count.items():
-        #     if freq == 1:
-        #         c += 1
-        #     if c > 2:
-        #         return False
-        # return True
-                
+  
         n = len(s)
         start = 0
         end = n-1
@@ -21,7 +12,6 @@ class Solution:
                 c += 1
                 newstr1 = s[0:start+1] + s[start+1:end] + s[end+1:]
                 newstr2 = s[0:start] + s[start+1:end] + s[end:]
-                print(newstr1, newstr2)
                 if newstr1 == newstr1[::-1] or newstr2 == newstr2[::-1]:
                     return True
                 else:
