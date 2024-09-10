2c417d3aa3c14ee9ed754f697a7ccf4c8a75cffa
Megha Singh <145432042+himeghasingh@users.noreply.github.com>
2024-09-10 03:26:07 -0400

    Time: 2425 ms (15.07%) | Memory: 58.2 MB (15.90%) - LeetSync

---
+++ 2035-count-sub-islands/count-sub-islands.py
@@ -0,0 +1,59 @@
+class Solution:
+    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+
+        def findIsland(i, j, islandList, grid):
+                for idir, jdir in directions:
+                    newi = i + idir
+                    newj = j + jdir
+                    if (0 <= newi < rows) and (0 <= newj < cols) and (grid[newi][newj] == 1) and (newi,newj) not in visited:
+                        islandList.append([newi, newj])
+                        visited.add((newi, newj))
+                        findIsland(newi, newj, islandList, grid)
+                return islandList, visited
+
+        rows = len(grid1)
+        cols = len(grid1[0])
+        directions = [[1,0], [-1,0], [0,1], [0,-1]]
+        # visited = set()
+        # grid1islands = []
+        # for i in range(0, rows):
+        #     for j in range(0, cols):
+        #         if grid1[i][j] == 1 and (i,j) not in visited: 
+        #             visited.add((i,j))
+        #             island, visited = findIsland(i, j, [[i,j]], grid1)
+        #             grid1islands.append(island)
+
+        visited = set()
+        grid2islands = []
+        for i in range(0, rows):
+            for j in range(0, cols):
+                if grid2[i][j] == 1 and (i,j) not in visited: 
+                    visited.add((i,j))
+                    island, visited = findIsland(i, j, [[i,j]], grid2)
+                    grid2islands.append(island)
+
+        c = 0
+        for islands in grid2islands:
+            flag = 0
+            for i, j in islands:
+                if grid1[i][j] != 1:
+                    flag = 1
+                    break
+            if flag == 0:
+                c += 1
+
+
+        # for row2 in grid2islands:
+        #     for row1 in grid1islands:
+        #         if set(map(tuple, row2)).issubset(set(map(tuple, row1))):
+        #             c += 1
+        return c
+
+
+
+                    
+
+
+            
+
+        
