9e6290b16d80ccbb3219417f570bbacbce36891d
Megha Singh <145432042+himeghasingh@users.noreply.github.com>
2024-09-11 03:20:37 -0400

    Time: 60 ms (54.57%) | Memory: 27.9 MB (60.42%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/repeated-dna-sequences.py
@@ -1,16 +1,21 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         n = len(s)
+        res = []
         strMap = defaultdict(int)
+        seen = set()
+
         if n <= 10:
             return []
-        for i in range(0, n-9):
+        for i in range(0, n - 10 + 1):
             substr = s[i:i+10]
             strMap[substr] += 1
-        res = []
-        for k, v in strMap.items():
-            if v > 1:
-                res.append(k)
+            
+            if strMap[substr] > 1:
+                if substr not in seen:
+                    res.append(substr)
+                    seen.add(substr)
+            
         return res
 
 
