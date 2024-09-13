b3cb32d07bb10140e9ea0c9b78f54c2bf524e5ab
Megha Singh <145432042+himeghasingh@users.noreply.github.com>
2024-09-13 01:29:39 -0400

    Time: 553 ms (97.22%) | Memory: 23.1 MB (41.94%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -2,22 +2,59 @@ class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
 
         n = len(points)
-        res = []
+        # res = []
 
-        dist = defaultdict(int)
-        for i in range(0, n):
+        # dist = defaultdict(int)
+        # for i in range(0, n):
+        #     x, y = points[i]
+        #     dist[i] = (x * x) + (y * y)
+
+        # sortedDist = dict(sorted(dist.items(), key=lambda item: item[1]))
+        # indexes = list(sortedDist.keys())[0:k]
+
+        # i = 0
+        # while(k > 0):
+        #     res.append(points[indexes[i]])
+        #     k -= 1
+        #     i += 1
+
+        # return res
+
+        heap = []
+        for i in range(0, k):
             x, y = points[i]
-            dist[i] = (x * x) + (y * y)
+            distance =  -1 * ((x * x) + (y * y))
+            heap.append((distance, points[i]))
+        heapq.heapify(heap)
 
-        sortedDist = dict(sorted(dist.items(), key=lambda item: item[1]))
-        indexes = list(sortedDist.keys())[0:k]
+        for i in range(k, n):
+            x, y = points[i]
+            distance =  -1 * ((x * x) + (y * y))
+            if distance > heap[0][0]:
+                heapq.heappop(heap)
+                heapq.heappush(heap, (distance, points[i]))
 
-        i = 0
-        while(k > 0):
-            res.append(points[indexes[i]])
-            k -= 1
-            i += 1
-
+        res = []
+        for i in range(0, len(heap)):
+            res.append(heap[i][1])
         return res
+
+
+
+
+        
+    #     heap = [(-self.squared_distance(points[i]), i) for i in range(k)]
+    #     heapq.heapify(heap)
+    #     for i in range(k, len(points)):
+    #         dist = -self.squared_distance(points[i])
+    #         heapq.heappop()
+    #         heapq.heappush()
+        
+    #     # Return all points stored in the max heap
+    #     return [points[i] for (_, i) in heap]
+    
+    # def squared_distance(self, point: List[int]) -> int:
+    #     """Calculate and return the squared Euclidean distance."""
+    #     return point[0] ** 2 + point[1] ** 2
 
     
