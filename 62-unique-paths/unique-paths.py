8ed9b06546e1a62eca66b440aea797b70a013e2f
Megha Singh <145432042+himeghasingh@users.noreply.github.com>
2024-09-13 01:51:30 -0400

    Time: 40 ms (23.43%) | Memory: 16.4 MB (76.69%) - LeetSync

---
+++ 62-unique-paths/unique-paths.py
@@ -0,0 +1,51 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+
+        ##########DP SOLUTION##########
+        dp = [[1] * n for i in range(m)]
+
+        for col in range(1, m):
+            for row in range(1, n):
+                dp[col][row] = dp[col - 1][row] + dp[col][row - 1]
+
+        return dp[m - 1][n - 1]
+
+
+
+        ##########RECURSIVE SOLUTION##########
+        # if m == 1 or n == 1:
+        #     return 1
+
+        # return self.uniquePaths(m - 1, n) + self.uniquePaths(m, n - 1)
+
+
+        ##########DFS SOLUTION##########
+        # def dfs(i, j, visited, paths):
+
+        #     if i == m - 1 and j == n - 1:
+        #         paths[0] += 1
+        #         return paths[0]
+
+        #     for idir, jdir in directions:
+        #         newi = i + idir
+        #         newj = j + jdir
+        #         if (0 <= newi < m) and (0 <= newj < n) and (newi, newj) not in visited:
+        #             visited.add((newi, newj))
+        #             dfs(newi, newj, visited, paths)
+        #             visited.remove((newi, newj))
+        #     return paths[0]
+
+        # directions = [[1,0],[0,1]]
+        # visited = set()
+        # visited.add((0,0))
+        # paths = [0]
+
+        # paths = dfs(0, 0, visited, paths)
+        # return paths
+
+
+
+        
+
+
+        
