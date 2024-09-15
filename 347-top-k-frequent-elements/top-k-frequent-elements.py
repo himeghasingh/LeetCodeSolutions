44b6b2fc3b69c49b13a26327e2415230761d09d0
Megha Singh <145432042+himeghasingh@users.noreply.github.com>
2024-09-15 03:23:31 -0400

    Time: 83 ms (84.47%) | Memory: 21.2 MB (45.08%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -4,5 +4,24 @@ class Solution:
         # count = dict(sorted(count.items(), key = lambda item:item[1], reverse=True))
         # res = list(count.keys())[0:k]
         # return res
-        return heapq.nlargest(k, count.keys(), count.get)
+        # return heapq.nlargest(k, count.keys(), count.get)
+        heap = []
+        heapq.heapify(heap)
+        i = 0
+
+        for key,val in count.items():
+            if len(heap) < k:
+                heapq.heappush(heap, [val, key])
+            else:
+                if val > heap[0][0]:
+                    heapq.heappop(heap)
+                    heapq.heappush(heap, [val, key])
+        res = []
+        print(heap)
+        for val, key in heap:
+            res.append(key)
+        return res
+
+
+
         
